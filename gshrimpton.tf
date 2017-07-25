f07d45da1fe0dea67cc35633a63e7f9c512b05b7
Will Benyon <git@benyon.io>
2017-07-25 22:52:10 +0100

    Add GetObject S3 Bucket Policy
    Not really sure how this was working previously...

--- gshrimpton.tf
+++ gshrimpton.tf
@@ -14,6 +14,24 @@ resource "aws_s3_bucket" "web_bucket" {
   bucket = "www.gshrimpton.com"
   acl    = "public-read"
 
+  policy = <<EOF
+{
+"Id": "bucket_policy_site",
+"Version": "2012-10-17",
+"Statement": [
+  {
+    "Sid": "bucket_policy_site_main",
+    "Action": [
+      "s3:GetObject"
+    ],
+    "Effect": "Allow",
+    "Resource": "arn:aws:s3:::www.gshrimpton.com/*",
+    "Principal": "*"
+  }
+]
+}
+EOF
+
   website {
     index_document = "index.html"
     error_document = "error.html"
